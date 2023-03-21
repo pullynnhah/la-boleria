164198815746b0742f25911ed4e88b2c0b258569
Paula Rodrigues <pullynnhah@icloud.com>
2023-03-21 19:41:30 -0300

    :sparkles: add error handling for uri

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -2,9 +2,11 @@ import { StatusCodes } from "http-status-codes";
 
 const validate = schema => (req, res, next) => {
   const validation = schema.validate(req.body);
-  console.log(validation.error);
-  if (validation.error) res.sendStatus(StatusCodes.BAD_REQUEST);
-  else next();
+  if (validation.error) {
+    if (validation.error.details.find(err => err.type === "string.uri"))
+      res.sendStatus(StatusCodes.UNPROCESSABLE_ENTITY);
+    else res.sendStatus(StatusCodes.BAD_REQUEST);
+  } else next();
 };
 
 export default validate;
